ef255985f142b86f75671d3623b894d3418df694
itamarstollman <itamar.stollman@mail.huji.ac.il>
2022-11-22 19:15:09 +0200

    show, gray_blur, canny_edge, normlize_img_size, captur_img

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
 import cv2
-
+import numpy as np
 
 print (cv2.__version__)
 
+def show(img):
+    cv2.imshow(str(img), img)
+    cv2.waitKey(0)
+
+def gray_blur(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_blur = cv2.blur(gray, (5, 5))
+    show(gray_blur)
+    return gray_blur
+
+# Canny edge detection
+def canny_edge(img):
+
+    c_edges = cv2.Canny(img, 90, 200)
+    show(c_edges)
+    return c_edges
+
+
+def normlize_img_size(img):
+
+    scale_percent = 60  # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    # resize image
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    show(resized)
+    return resized
 
 
 def captur_img(img):
 
     img = cv2.imread(str(img), 1)
-    cv2.imshow('current img', img)
-    cv2.waitKey(0)
+    show(img)
+
+    return img
 
 
-captur_img('chess_board_1.png')
+
+img = captur_img('chess_board_3.png')
+resized = normlize_img_size(img)
+gray_blur = gray_blur(resized)
+canny_edge = canny_edge(gray_blur)
+
+
