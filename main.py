197a54a3d3e41c053ae47dd0fe81f8f8dc90a6f2
itamarstollman <itamar.stollman@mail.huji.ac.il>
2022-11-22 18:36:55 +0200

    first checks

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import cv2
 
 
 print (cv2.__version__)
+
+
+
+def captur_img(img):
+
+    img = cv2.imread(str(img), 1)
+    cv2.imshow('current img', img)
+    cv2.waitKey(0)
+
+
+captur_img('chess_board_1.png')
